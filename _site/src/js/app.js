57d23da46cd571fbde93d0c9d84d7d06886db960
hyungsub08 <hyungsub08@gmail.com>
2020-03-05 01:37:44 +0900

    main time added

--- _site/src/js/app.js
+++ _site/src/js/app.js
@@ -2,3 +2,17 @@
 $(window).on("load", function() {
     // $('.screen-loading').delay(2500).fadeOut(500);
 });
+
+// scroll to top
+$(window).scroll(function() {
+    // if ($(this).scrollTop() >= 50) {       
+    //     $('#return-to-top').fadeIn(200);   
+    // } else {
+    //     $('#return-to-top').fadeOut(200);  
+    // }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
