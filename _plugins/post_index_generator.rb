d2b0f20d3a15b4f2b94c887f065aab699b3eaa1e
hyungsub08 <hyungsub08@gmail.com>
2020-03-23 21:43:18 +0900

    changes

---
+++ _plugins/post_index_generator.rb
@@ -0,0 +1,11 @@
+module Jekyll
+    class PostIndex < Generator
+        safe true
+        priority :low
+        def generate(site)
+            site.posts.each_with_index do |item, index|
+                item.data['index'] = index + 1
+            end
+        end
+    end
+end
